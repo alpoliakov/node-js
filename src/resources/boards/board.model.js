a5cdbf5082cf118cc21f4206d2e2b75fe6ed086a
alpoliakov <alpoliakov73@gmail.com>
2020-10-12 21:42:50 +0300

    fix: Fixed inaccuracies in the code.

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -10,10 +10,6 @@ class Board {
     this.title = title;
     this.columns = columns;
   }
-  static toResponse(board) {
-    const { id, title, columns } = board;
-    return { id, title, columns };
-  }
   static fromRequest(body) {
     return new Board(body);
   }
