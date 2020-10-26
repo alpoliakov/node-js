950b358f741531ff4c496cb9294d36d809163fd4
alpoliakov <alpoliakov73@gmail.com>
2020-10-27 01:07:46 +0200

    feat: Completion of the transfer to MongoDB

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,9 +1,16 @@
 const boardsRepo = require('./board.db.repository');
+const taskService = require('../tasks/task.service');
 
 const getAll = () => boardsRepo.getAll();
 const get = id => boardsRepo.get(id);
 const save = board => boardsRepo.save(board);
 const update = (id, board) => boardsRepo.update(id, board);
-const remove = id => boardsRepo.remove(id);
+const remove = async id => {
+  const isRemoved = await boardsRepo.remove(id);
+  if (isRemoved) {
+    await taskService.removeTasksFromBoard(id);
+    return isRemoved;
+  }
+};
 
 module.exports = { getAll, get, save, update, remove };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,14 +1,20 @@
 const usersRepo = require('./user.db.repository');
-// const User = require('./user.model');
+const taskService = require('../tasks/task.service');
 
 const getAll = () => usersRepo.getAll();
 
 const get = id => usersRepo.get(id);
 
-const remove = id => usersRepo.remove(id);
-
 const save = user => usersRepo.save(user);
 
 const update = (id, user) => usersRepo.update(id, user);
+
+const remove = async id => {
+  const isRemoved = await usersRepo.remove(id);
+  if (isRemoved) {
+    await taskService.unsubscribeFromTasks(id);
+    return isRemoved;
+  }
+};
 
 module.exports = { getAll, get, save, remove, update };
